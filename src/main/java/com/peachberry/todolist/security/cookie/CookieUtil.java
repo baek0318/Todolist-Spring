96defc574c6ee4640d49857dbafaead3796b515f
peachberry <peachberry0318@gmail.com>
2021-01-16 12:28:48 +0900

    Add CookieUtil class

--- src/main/java/com/peachberry/todolist/security/cookie/CookieUtil.java
+++ src/main/java/com/peachberry/todolist/security/cookie/CookieUtil.java
@@ -1,2 +1,19 @@
-package com.peachberry.todolist.security.cookie;public interface CookieUtil {
+package com.peachberry.todolist.security.cookie;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+public interface CookieUtil {
+
+    public Cookie createAccessCookie(String token, String name);
+
+    public Cookie createRefreshCookie(String token, String name);
+
+    public Cookie createLogoutRefreshCookie(String name);
+
+    public Cookie createLogoutAccessCookie(String name);
+
+    public Cookie getAccessCookie(HttpServletRequest request);
+
+    public Cookie getRefreshCookie(HttpServletRequest request);
 }
--- src/main/java/com/peachberry/todolist/security/cookie/CookieUtilImpl.java
+++ src/main/java/com/peachberry/todolist/security/cookie/CookieUtilImpl.java
@@ -1,2 +1,70 @@
-package com.peachberry.todolist.security.cookie;public class CookieUtilImpl {
+package com.peachberry.todolist.security.cookie;
+
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+@Service
+public class CookieUtilImpl implements CookieUtil{
+
+    private final int MAX_AGE = 3600000*24*14;
+
+    private final String ACCESS_TOKEN = "ACCESS-TOKEN";
+
+    private final String REFRESH_TOKEN = "REFRESH-TOKEN";
+
+    @Override
+    public Cookie createAccessCookie(String token, String name) {
+        Cookie cookie = new Cookie(name,token);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(MAX_AGE);
+        return cookie;
+    }
+
+    @Override
+    public Cookie createRefreshCookie(String token, String name) {
+        Cookie cookie = new Cookie(name,token);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/api/auth/issueAccess");
+        cookie.setMaxAge(MAX_AGE);
+        return cookie;
+    }
+
+    @Override
+    public Cookie createLogoutRefreshCookie(String name) {
+        Cookie cookie = new Cookie(name,"logout");
+        cookie.setHttpOnly(true);
+        cookie.setPath("/api/auth/issueAccess");
+        cookie.setMaxAge(0);
+        return cookie;
+    }
+
+    @Override
+    public Cookie createLogoutAccessCookie(String name) {
+        Cookie cookie = new Cookie(name,"logout");
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        return cookie;
+    }
+
+    @Override
+    public Cookie getAccessCookie(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        return cookies != null ? getCookie(cookies, ACCESS_TOKEN) : null;
+    }
+
+    @Override
+    public Cookie getRefreshCookie(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        return cookies != null ? getCookie(cookies, REFRESH_TOKEN) : null;
+    }
+
+    private Cookie getCookie(Cookie[] cookies, String type) {
+        return Arrays.stream(cookies).filter(cookie -> cookie.getName().equals(type)).findFirst()
+                .orElse(null);
+    }
 }
