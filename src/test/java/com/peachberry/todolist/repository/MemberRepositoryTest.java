1f446fb73b82f6f5d0d2dee3e06c2c925f297edd
peachberry <peachberry0318@gmail.com>
2021-01-17 15:21:48 +0900

    Add MemberRepositoryTest class

--- src/test/java/com/peachberry/todolist/repository/MemberRepositoryTest.java
+++ src/test/java/com/peachberry/todolist/repository/MemberRepositoryTest.java
@@ -1,2 +1,33 @@
-package com.peachberry.todolist.repository;public class MemberRepositoryTest {
+package com.peachberry.todolist.repository;
+
+
+import com.peachberry.todolist.domain.Authority;
+import com.peachberry.todolist.domain.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback
+    //제대로 저장이 되는지 확인
+    void saveAndLoadMemberData() {
+        //given
+        /*
+        String email = "baek0318@icloud.com";
+        String password = "1234";
+        String name = "baek";
+        Authority authority = new Authority();
+        Member member = new Member();
+
+        //when
+        memberRepository.save();*/
+
+        //then
+    }
 }
