985beb0691c80fc2250d5e25aeda837264792db2
peachberry <peachberry0318@gmail.com>
2021-01-18 12:44:57 +0900

    Add MemberRepositoryTest

--- src/test/java/com/peachberry/todolist/repository/MemberRepositoryTest.java
+++ src/test/java/com/peachberry/todolist/repository/MemberRepositoryTest.java
@@ -3,31 +3,26 @@ package com.peachberry.todolist.repository;
 
 import com.peachberry.todolist.domain.Authority;
 import com.peachberry.todolist.domain.Member;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
 public class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
 
     @Test
-    @Transactional
     @Rollback
-    //제대로 저장이 되는지 확인
+    @DisplayName("제대로 저장 검색되는지 확인")
     void saveAndLoadMemberData() {
-        //given
-        /*
-        String email = "baek0318@icloud.com";
-        String password = "1234";
-        String name = "baek";
-        Authority authority = new Authority();
-        Member member = new Member();
 
-        //when
-        memberRepository.save();*/
-
-        //then
     }
 }
