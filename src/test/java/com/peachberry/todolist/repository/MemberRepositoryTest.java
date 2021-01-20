69ab5e5ad95a3cb53548e1b99198d7dd77928701
peachberry <peachberry0318@gmail.com>
2021-01-20 15:49:32 +0900

    Add MemberRepositoryTest testcase
    check vaild email
    check save and find member

--- src/test/java/com/peachberry/todolist/repository/MemberRepositoryTest.java
+++ src/test/java/com/peachberry/todolist/repository/MemberRepositoryTest.java
@@ -3,26 +3,88 @@ package com.peachberry.todolist.repository;
 
 import com.peachberry.todolist.domain.Authority;
 import com.peachberry.todolist.domain.Member;
+import com.peachberry.todolist.domain.Role;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.validation.*;
+import java.util.Set;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Transactional
 public class MemberRepositoryTest {
 
-    @Autowired MemberRepository memberRepository;
+    private Logger logger = LoggerFactory.getLogger(MemberRepositoryTest.class);
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private AuthorityRepository authorityRepository;
+
+    private final String EMAIL = "baek0318@icloud.com";
+    private final String PASSWORD = "12345678910";
+    private final String NAME = "PEACHBERRY";
+    private final Authority authority = new Authority(Role.USER);
+
+    private Validator validator;
+
+    @BeforeEach
+    void saveAuthority() {
+        authorityRepository.save(authority);
+    }
+
+    @BeforeEach
+    void setValidator() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
 
     @Test
     @Rollback
     @DisplayName("제대로 저장 검색되는지 확인")
     void saveAndLoadMemberData() {
+        //given
+        Member member = new Member(EMAIL, PASSWORD, NAME, authority);
+
+        //when
+        memberRepository.save(member);
+        Member result = memberRepository.findByEmail(EMAIL);
+
+        //then
+        Assertions.assertThat(result).isEqualTo(member);
+    }
+
+    //올바르지 않은 이메일 형식을 입력하였을때
+    @Test
+    @Rollback
+    @DisplayName("이메일 형식이 올바르지 않습니다")
+    void checkEmailValidation() {
+        //given
+        String wrong = "baek0318@.com";
+        Member member = new Member(wrong ,PASSWORD,NAME,authority);
+
+        //when
+        Set<ConstraintViolation<Member>> validates = validator.validate(member);
+
+        //then
+        Assertions.assertThat(validates.iterator().next().getInvalidValue()).isEqualTo(wrong);
+    }
+
+    //동일한 이메일이 존재하는지 확인
+    @Test
+    @Rollback
+    @DisplayName("동일한 이메일 존재")
+    void checkSameEmail() {
         //given
 
         //when
