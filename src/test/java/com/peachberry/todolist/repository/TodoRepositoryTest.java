978aee480bf69a43875685f1d638910e2cca53a3
peachberry <peachberry0318@gmail.com>
2021-01-25 12:41:13 +0900

    Add TodoRepositoryTest.java

---
+++ src/test/java/com/peachberry/todolist/repository/TodoRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.peachberry.todolist.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {})
+class TodoRepositoryTest {
+
+    @Test
+    @DisplayName("저장이 제대로 되는지 확인")
+    void save() {
+    }
+
+    @Test
+    @DisplayName("")
+    void findAll() {
+    }
+
+    @Test
+    @DisplayName("")
+    void findById() {
+    }
+
+    @Test
+    @DisplayName("")
+    void findByStatus() {
+    }
+
+    @Test
+    @DisplayName("")
+    void findByCalendar() {
+    }
+
+    @Test
+    @DisplayName("")
+    void reviseStatus() {
+    }
+
+    @Test
+    @DisplayName("")
+    void reviseTodo() {
+    }
+
+    @Test
+    @DisplayName("")
+    void reviseCalendar() {
+    }
+
+    @Test
+    @DisplayName("")
+    void deleteById() {
+    }
+}
