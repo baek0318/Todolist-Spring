15a3e551478301e5b161b465ed4b6509c6954bbd
peachberry <peachberry0318@gmail.com>
2021-01-29 12:32:08 +0900

    Update findByRole return type
    findByRole에서 반환타입에 null을 할 수 있도록 만들었었는데
    null을 반환하는 것 보다 비어있는 리스트를 반환하는게 좋다고 판단되어
    반환 값을 바꾸게 되었다

--- src/main/java/com/peachberry/todolist/repository/AuthorityRepository.java
+++ src/main/java/com/peachberry/todolist/repository/AuthorityRepository.java
@@ -11,6 +11,7 @@ import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 
 @Repository
 public class AuthorityRepository {
@@ -24,25 +25,10 @@ public class AuthorityRepository {
      * authority를 DB에 저장해준다
      * @param authority authority객체를 넘겨주면 된다
      */
-    public void save(Authority authority) {
+    public Long save(Authority authority) {
         em.persist(authority);
         logger.info("member save success");
-    }
-
-    /**
-     * 주어진 Role을 바탕으로 해당 Role이 존재하는지 확인
-     * @param role 확인하고자하는 Role
-     * @return 있다면 true 없다면 false
-     */
-    public Boolean existByRole(Role role) {
-        if(findByRole(role) != null) {
-            logger.error("member already exist");
-            return true;
-        }
-        else {
-            logger.info("member doesn't exist");
-            return false;
-        }
+        return authority.getId();
     }
 
     /**
@@ -50,16 +36,9 @@ public class AuthorityRepository {
      * @param role 찾고자하는 Role
      * @return 찾았다면 Authority 객체를 넘겨준다 아니면 null을 넘긴다
      */
-    public Authority findByRole(Role role) {
-        try {
-            Authority authority = em.createQuery("select a from Authority a where a.role = :role", Authority.class)
-                    .setParameter("role", role)
-                    .getSingleResult();
-            logger.info("success find member");
-            return authority;
-        }catch(NoResultException e){
-            logger.error("can not find role {}" ,e.getMessage());
-            return null;
-        }
+    public List<Authority> findByRole(Role role) {
+        return em.createQuery("select a from Authority a where a.role = :role", Authority.class)
+                .setParameter("role", role)
+                .getResultList();
     }
 }
