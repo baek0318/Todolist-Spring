2cf871b2119fd5f9970d6f508a5fd7824f48e70a
peachberry <peachberry0318@gmail.com>
2021-01-30 19:38:12 +0900

    Complete SignOutDTO.java using builder

--- src/main/java/com/peachberry/todolist/dto/SignUpDTO.java
+++ src/main/java/com/peachberry/todolist/dto/SignUpDTO.java
@@ -1,4 +1,25 @@
 package com.peachberry.todolist.dto;
 
+import com.peachberry.todolist.domain.Authority;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class SignUpDTO {
+
+    private final String email;
+
+    private final String password;
+
+    private final String authority;
+
+    private final String name;
+
+    public SignUpDTO(String email, String password, String name, String authority) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.authority = authority;
+    }
 }
