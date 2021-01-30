109088b09f049acaa85d4e5a99073741b16c8c3a
peachberry <peachberry0318@gmail.com>
2021-01-30 21:39:36 +0900

    Complete CookieDTO.java

--- src/main/java/com/peachberry/todolist/dto/CookieDTO.java
+++ src/main/java/com/peachberry/todolist/dto/CookieDTO.java
@@ -1,4 +1,15 @@
 package com.peachberry.todolist.dto;
 
+import javax.servlet.http.Cookie;
+
 public class CookieDTO {
+
+    private final Cookie accessCookie;
+
+    private final Cookie refreshCookie;
+
+    public CookieDTO(Cookie accessCookie, Cookie refreshCookie) {
+        this.accessCookie = accessCookie;
+        this.refreshCookie = refreshCookie;
+    }
 }
