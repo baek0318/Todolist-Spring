e0e7999c00d3dc7bb7f72ed6c6459da2704a8eb1
peachberry <peachberry0318@gmail.com>
2021-02-04 13:22:03 +0900

    Add TodoService @Transactional Annotation

--- src/main/java/com/peachberry/todolist/service/TodoService.java
+++ src/main/java/com/peachberry/todolist/service/TodoService.java
@@ -6,6 +6,7 @@ import com.peachberry.todolist.repository.TodoRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,42 +21,52 @@ public class TodoService {
         this.todoRepository = todoRepository;
     }
 
+    @Transactional
     public void saveTodo(Todo todo) {
         todoRepository.save(todo);
     }
 
+    @Transactional
     public List<Todo> findAllTodo(Member member) {
         return todoRepository.findAll(member);
     }
 
+    @Transactional
     public Todo findTodoById(Todo todo) {
         return todoRepository.findById(todo.getId());
     }
 
+    @Transactional
     public List<Todo> findTodoByStatus(Todo todo, Member member) {
         return todoRepository.findByStatus(todo.getStatus(), member);
     }
 
+    @Transactional
     public List<Todo> findTodoByCalendar(Todo todo, Member member) {
         return todoRepository.findByCalendar(todo.getCalendar(), member);
     }
 
+    @Transactional
     public List<Todo> findTodoByCategory(Todo todo, Member member) {
         return todoRepository.findByCategory(todo.getCategory(), member);
     }
 
+    @Transactional
     public void reviseTodoByStatus(Todo todo) {
         todoRepository.reviseStatus(todo.getStatus(), todo.getId());
     }
 
+    @Transactional
     public void reviseTodoByTitle(Todo todo) {
         todoRepository.reviseTodo(todo.getTitle(), todo.getId());
     }
 
+    @Transactional
     public void reviseTodoByCalendar(Todo todo) {
         todoRepository.reviseCalendar(todo.getCalendar(), todo.getId());
     }
 
+    @Transactional
     public void deleteTodo(Todo todo) {
         todoRepository.deleteById(todo.getId());
     }
