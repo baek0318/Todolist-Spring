dbcc0b70f0c4c9fd66c2a3e1e19ac6b88b684307
peachberry <peachberry0318@gmail.com>
2021-02-07 22:13:37 +0900

    Add SignUpSuccessDTO.java

--- src/main/java/com/peachberry/todolist/dto/response/SignUpSuccessDTO.java
+++ src/main/java/com/peachberry/todolist/dto/response/SignUpSuccessDTO.java
@@ -1,6 +1,7 @@
 package com.peachberry.todolist.dto.response;
 
 import com.peachberry.todolist.domain.Authority;
+import com.peachberry.todolist.domain.Role;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,7 +13,7 @@ public class SignUpSuccessDTO {
 
     private final String name;
 
-    private final Authority authority;
+    private final Role role;
 
     private final Long id;
 
