da63a93d060b188715e1d9e038ed16ca8e09a652
peachberry <peachberry0318@gmail.com>
2021-02-09 11:54:24 +0900

    Fix Test Integration -> less Integration beacuse no db

--- src/test/java/com/peachberry/todolist/controller/AuthenticationControllerTest.java
+++ src/test/java/com/peachberry/todolist/controller/AuthenticationControllerTest.java
@@ -1,60 +1,47 @@
 package com.peachberry.todolist.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.peachberry.todolist.AppConfig;
+import com.peachberry.todolist.domain.Member;
 import com.peachberry.todolist.domain.Role;
 import com.peachberry.todolist.dto.request.SignUpDTO;
 import com.peachberry.todolist.dto.response.SignUpSuccessDTO;
-import com.peachberry.todolist.dto.response.SuccessResponseDTO;
-import com.peachberry.todolist.security.cookie.CookieUtil;
-import com.peachberry.todolist.security.domain.UserDetailsImpl;
+import com.peachberry.todolist.security.WebSecurityConfig;
+import com.peachberry.todolist.security.cookie.CookieUtilImpl;
 import com.peachberry.todolist.security.jwt.JwtAuthEntryPoint;
 import com.peachberry.todolist.security.jwt.JwtAuthTokenFilter;
 import com.peachberry.todolist.security.jwt.JwtUtil;
 import com.peachberry.todolist.security.service.UserDetailsServiceImpl;
 import com.peachberry.todolist.service.AuthenticationService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
 
 import static org.mockito.BDDMockito.given;
+
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-@WebMvcTest
+
+@WebMvcTest(value = AuthenticationController.class, includeFilters = @ComponentScan.Filter(classes = {EnableWebSecurity.class}))
+@Import({JwtAuthTokenFilter.class})
 public class AuthenticationControllerTest {
 
     @Autowired
@@ -63,13 +50,20 @@ public class AuthenticationControllerTest {
     private ObjectMapper objectMapper;
     @MockBean
     private AuthenticationService authenticationService;
+    @MockBean
+    private JwtUtil jwtUtil;
+    @MockBean
+    private CookieUtilImpl cookieUtil;
+    @MockBean
+    private UserDetailsServiceImpl userDetailsService;
+    @MockBean
+    private JwtAuthEntryPoint jwtAuthEntryPoint;
 
 
-    private final SignUpDTO signUpDTO = new SignUpDTO("baek0318@icloud.com", "1234", "baek", "USER");
-
+    private final SignUpDTO signUpDTO = new SignUpDTO("peachberry@kakao.com", "1234", "peach", "USER");
 
     @Test
-    @DisplayName("로그인을 했을시 유효한 유저인지 검사")
+    @DisplayName("회원가입을 진행시에 올바른 응답값이 나오는지 확인")
     @WithMockUser
     void signUpTest() throws Exception {
 
@@ -77,8 +71,8 @@ public class AuthenticationControllerTest {
 
         given(authenticationService.signup(signUpDTO))
                 .willReturn(SignUpSuccessDTO.builder()
-                        .email("baek0318@icloud.com")
-                        .name("baek")
+                        .email("peachberry@kakao.com")
+                        .name("peach")
                         .id(1L)
                         .role(Role.USER).build());
 
@@ -88,19 +82,6 @@ public class AuthenticationControllerTest {
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(print());
-
-    }
-
-    @Test
-    @DisplayName("회원가입을 했을시 중복체크")
-    void signUpDuplicateTest() throws Exception {
-
-        String content = objectMapper.writeValueAsString(signUpDTO);
-
-        mockMvc.perform(post("/api/auth/signup")
-                .content(content)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        verify(authenticationService, times(1)).signup(signUpDTO);
     }
 }
