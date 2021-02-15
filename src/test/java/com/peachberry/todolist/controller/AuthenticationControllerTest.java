8e3c939ca6fcf7d547374ce31a3580ca3fce5a9d
peachberry <peachberry0318@gmail.com>
2021-02-15 13:23:28 +0900

    Add SignIn Unit test for controller

--- src/test/java/com/peachberry/todolist/controller/AuthenticationControllerTest.java
+++ src/test/java/com/peachberry/todolist/controller/AuthenticationControllerTest.java
@@ -1,6 +1,10 @@
 package com.peachberry.todolist.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.peachberry.todolist.dto.CookieDTO;
+import com.peachberry.todolist.dto.request.SignInDTO;
+import com.peachberry.todolist.security.cookie.CookieUtil;
+import com.peachberry.todolist.service.exception.SignInFailException;
 import com.peachberry.todolist.service.exception.SignUpFailException;
 import com.peachberry.todolist.domain.Role;
 import com.peachberry.todolist.dto.request.SignUpDTO;
@@ -23,6 +27,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.test.web.servlet.MockMvc;
 
 
+import javax.servlet.http.Cookie;
+
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 
@@ -34,7 +40,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @WebMvcTest(value = AuthenticationController.class, includeFilters = @ComponentScan.Filter(classes = {EnableWebSecurity.class}))
-@Import({JwtAuthTokenFilter.class})
+@Import({JwtAuthTokenFilter.class, CookieUtilImpl.class})
 public class AuthenticationControllerTest {
 
     @Autowired
@@ -43,17 +49,20 @@ public class AuthenticationControllerTest {
     private ObjectMapper objectMapper;
     @MockBean
     private AuthenticationService authenticationService;
+    @Autowired
+    private JwtAuthTokenFilter jwtAuthTokenFilter;
     @MockBean
     private JwtUtil jwtUtil;
-    @MockBean
-    private CookieUtilImpl cookieUtil;
+    @Autowired
+    private CookieUtil cookieUtil;
     @MockBean
     private UserDetailsServiceImpl userDetailsService;
     @MockBean
     private JwtAuthEntryPoint jwtAuthEntryPoint;
 
-
     private final SignUpDTO signUpDTO = new SignUpDTO("peachberry@kakao.com", "1234", "peachberry", "USER");
+
+    private final SignInDTO signInDTO = new SignInDTO("peachberry@kakao.com", "1234");
 
     @Test
     @DisplayName("회원가입을 진행시에 올바른 응답값이 나오는지 확인")
@@ -84,7 +93,7 @@ public class AuthenticationControllerTest {
     void testSignUp_if_signup_fail() throws Exception {
         String content = objectMapper.writeValueAsString(signUpDTO);
 
-        given(authenticationService.signup(any())).willThrow(new SignUpFailException());
+        given(authenticationService.signup(any())).willThrow(new SignUpFailException("해당 이메일이 존재합니다"));
 
         mockMvc.perform(post("/api/auth/signup")
                 .accept(MediaType.APPLICATION_JSON)
@@ -92,4 +101,44 @@ public class AuthenticationControllerTest {
                 .content(content))
                 .andExpect(status().isUnauthorized());
     }
+
+    @Test
+    @DisplayName("로그인이 성공한 경우")
+    void testSignIn() throws Exception {
+
+        String content = objectMapper.writeValueAsString(signInDTO);
+
+        Cookie access = cookieUtil.createAccessCookie("");
+        Cookie refresh = cookieUtil.createRefreshCookie("");
+
+        given(authenticationService.signin(any()))
+                .willReturn(new CookieDTO(access, refresh));
+
+        mockMvc.perform(post("/api/auth/signin")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content))
+                .andExpect(jsonPath("response").value("SignIn Success"))
+                .andExpect(status().isOk())
+                .andExpect(cookie().httpOnly("ACCESS-TOKEN", true))
+                .andExpect(cookie().httpOnly("REFRESH-TOKEN", true))
+                .andExpect(cookie().exists("ACCESS-TOKEN"))
+                .andExpect(cookie().exists("REFRESH-TOKEN"));
+
+    }
+
+    @Test
+    @DisplayName("로그인이 실패한 경우")
+    void testSignIn_Failed() throws Exception {
+        String content = objectMapper.writeValueAsString(signInDTO);
+
+        given(authenticationService.signin(any())).willThrow(new SignInFailException("로그인에 실패했습니다"));
+
+        mockMvc.perform(post("/api/auth/signin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(content))
+                .andExpect(status().isUnauthorized());
+    }
+
 }
