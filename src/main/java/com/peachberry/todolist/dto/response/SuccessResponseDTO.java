e51262ef8bb541c5714af67b64fb0230b4fab512
peachberry <peachberry0318@gmail.com>
2021-02-16 23:53:44 +0900

    Fix jackson json parsing error

--- src/main/java/com/peachberry/todolist/dto/response/SuccessResponseDTO.java
+++ src/main/java/com/peachberry/todolist/dto/response/SuccessResponseDTO.java
@@ -37,9 +37,4 @@ public class SuccessResponseDTO {
         return this.additionalProperties;
     }
 
-    @JsonAnySetter
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-    }
-
 }
