caa56aa884a7e982de606a932d341cc8d190e48c
peachberry <peachberry0318@gmail.com>
2021-02-24 19:08:52 +0900

    Add new CategorySaveFail exception

---
+++ src/main/java/com/peachberry/todolist/service/exception/CategorySaveFail.java
@@ -0,0 +1,8 @@
+package com.peachberry.todolist.service.exception;
+
+public class CategorySaveFail extends RuntimeException {
+
+    public CategorySaveFail(String message) {
+        super(message);
+    }
+}
