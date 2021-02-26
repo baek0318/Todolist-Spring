c84692af19113b5323496c83b07a3a3445e3c8fb
peachberry <peachberry0318@gmail.com>
2021-02-26 18:52:09 +0900

    카테고리 업데이트가 실패한 경우

---
+++ src/main/java/com/peachberry/todolist/service/exception/CategoryUpdateFail.java
@@ -0,0 +1,8 @@
+package com.peachberry.todolist.service.exception;
+
+public class CategoryUpdateFail extends RuntimeException {
+
+    public CategoryUpdateFail(String message) {
+        super(message);
+    }
+}
