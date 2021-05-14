3c3c617e0cbf9c4b79098762afd180486ea7edb2
peachberry <peachberry0318@gmail.com>
2021-05-14 18:34:46 +0900

    feat(Category) : Category 인수 테스트를 위한 데이터셋 생성 클래스 작성

--- src/test/java/com/peachberry/todolist/data/CategoryData.java
+++ src/test/java/com/peachberry/todolist/data/CategoryData.java
@@ -1,5 +1,24 @@
 package com.peachberry.todolist.data;
 
+import com.peachberry.todolist.domain.Category;
+import com.peachberry.todolist.domain.Member;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Component
 public class CategoryData {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Transactional
+    public Long saveCategory(String title, Long userId) {
+        Member member = em.find(Member.class, userId);
+        Category category = new Category(title, member);
+        em.persist(category);
+        return category.getId();
+    }
 }
