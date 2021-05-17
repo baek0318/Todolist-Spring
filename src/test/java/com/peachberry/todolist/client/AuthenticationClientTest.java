2541742ecc8d82afbc57c8e360115a5b686a8ad4
peachberry <peachberry0318@gmail.com>
2021-05-17 22:51:38 +0900

    refactor(Auth) : AuthenticationClientTest 테스트 무결성을 위해서 리팩토링

--- src/test/java/com/peachberry/todolist/client/AuthenticationClientTest.java
+++ src/test/java/com/peachberry/todolist/client/AuthenticationClientTest.java
@@ -1,5 +1,10 @@
 package com.peachberry.todolist.client;
 
+import com.peachberry.todolist.controller.dto.MemberResponse;
+import com.peachberry.todolist.data.AuthorityData;
+import com.peachberry.todolist.data.DatabaseCleanup;
+import com.peachberry.todolist.data.MemberData;
+import com.peachberry.todolist.domain.Member;
 import com.peachberry.todolist.domain.Role;
 import com.peachberry.todolist.controller.dto.auth.SignInRequest;
 import com.peachberry.todolist.controller.dto.auth.SignUpRequest;
@@ -15,12 +20,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AuthenticationClientTest {
 
     @Autowired
@@ -32,34 +38,56 @@ public class AuthenticationClientTest {
     @Autowired
     private CookieUtil cookieUtil;
 
-    private final SignUpRequest signUpRequest = new SignUpRequest("peachberry3@kakao.com", "1234", "peachberry");
+    @Autowired
+    private AuthorityData authorityData;
 
-    private final SignInRequest signInRequest = new SignInRequest("peachberry2@kakao.com", "1234");
+    @Autowired
+    private MemberData memberData;
+
+    @Autowired
+    private DatabaseCleanup dbClean;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    private final SignUpRequest signUpRequest = new SignUpRequest("peach@kakao.com", "peachberry", "1234");
+
+    private final SignInRequest signInRequest = new SignInRequest("peach@kakao.com", "1234");
 
     private final Logger logger = LoggerFactory.getLogger(AuthenticationClientTest.class);
 
     private String refresh = "empty";
 
+    @BeforeEach
+    void setUp() {
+        authorityData.saveAuthority(Role.USER);
+    }
+
+    @AfterEach
+    void tearDown() {
+        dbClean.execute();
+    }
+
     @Test
-    @Order(1)
     @DisplayName("회원가입 통합 테스트")
     void testSignUp() {
-
+        //given
         HttpHeaders headers = new HttpHeaders();
 
         HttpEntity<SignUpRequest> request = new HttpEntity<>(signUpRequest, headers);
 
         ResponseEntity<SignUpResponse> response = restTemplate
                 .postForEntity("/auth/signup",request , SignUpResponse.class);
-
+        System.out.println("ID : "+response.getBody().getId());
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody().getId()).isEqualTo(2L);
+        assertThat(response.getBody().getId()).isEqualTo(1L);
     }
 
     @Test
-    @Order(2)
     @DisplayName("로그인 통합 테스트")
     void testSignIn() {
+        //given
+        Long userId = memberData.saveMember("peach@kakao.com", "1234", "peach");
 
         HttpHeaders headers = new HttpHeaders();
 
@@ -74,6 +102,7 @@ public class AuthenticationClientTest {
         String refresh_header = cookies.get(2).split("=")[0]; //refresh_token
         String refresh_token = cookies.get(2).split(";")[0].split("=")[1]; //refresh_token
 
+        System.out.println("ID : "+userId);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(access_header).isEqualTo("ACCESS-TOKEN");
         assertThat(refresh_header).isEqualTo("REFRESH-TOKEN");
@@ -103,6 +132,7 @@ public class AuthenticationClientTest {
     @Test
     @DisplayName("ACCESS 토큰 재발행하기 통합테스트")
     void testIssueAccessToken() {
+        Long userId = memberData.saveMember("peach@kakao.com", "1234", "peach");
 
         HttpHeaders headers2 = new HttpHeaders();
 
